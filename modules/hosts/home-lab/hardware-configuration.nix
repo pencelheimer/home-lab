acc0026d5748989ac60f5b3b597c2047bf45815a
pencelheimer <pencelheimer@proton.me>
2026-08-29 13:50:18 +0300

    style(hardware-configuration): removed extra spaces

--- modules/hosts/home-lab/hardware-configuration.nix
+++ modules/hosts/home-lab/hardware-configuration.nix
@@ -24,25 +24,25 @@
       { device = "/dev/disk/by-uuid/1bca2463-54c0-4016-9706-f714de3c51c1";
         fsType = "btrfs";
       };
- 
+
     fileSystems."/home" =
       { device = "/dev/disk/by-uuid/1bca2463-54c0-4016-9706-f714de3c51c1";
         fsType = "btrfs";
         options = [ "subvol=home" ];
       };
- 
+
     fileSystems."/nix" =
       { device = "/dev/disk/by-uuid/1bca2463-54c0-4016-9706-f714de3c51c1";
         fsType = "btrfs";
         options = [ "subvol=nix" ];
       };
- 
+
     fileSystems."/boot" =
       { device = "/dev/disk/by-uuid/AAA7-97B9";
         fsType = "vfat";
         options = [ "fmask=0077" "dmask=0077" ];
       };
- 
+
     swapDevices =
       [ { device = "/dev/disk/by-uuid/8ebcf856-740b-4f2f-b86b-6eb7a890cb2f"; }
       ];
